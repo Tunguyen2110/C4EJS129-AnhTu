3538ef880ce3824678262aacb371e082b80b33db
Tu Nguyen <tunguyen211098@gmail.com>
2022-07-06 21:18:49 +0700

    update session 7

---
+++ session-7/index.js
@@ -0,0 +1,27 @@
+// alert('Go fuck yourself');
+// let a = 10;
+// console.log(a);
+// a = 20;
+// console.log(a);
+
+// const a = 10;
+// console.log(a);
+// a = 20;
+// console.log(a);
+
+// const a;
+// console.log(a);
+
+console.log('Var vs LET & CONST');
+{
+var course1 = "C 1";
+let course2 = "c 2";
+const course3 = "c 3";
+console.log(course1);
+console.log(course2);
+console.log(course3);
+}
+
+console.log(course1);  //only this shit appear cuz it can get out of the scope
+console.log(course2); // get out of memory == never existed;
+console.log(course3); // get out of memory never existed;
