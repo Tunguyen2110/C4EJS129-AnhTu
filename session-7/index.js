5d96c09b84cc476558deb34306bf87575b750fde
Tu Nguyen <tunguyen211098@gmail.com>
2022-07-07 23:18:23 +0700

    update linh tinh

--- session-7/index.js
+++ session-7/index.js
@@ -12,16 +12,41 @@
 // const a;
 // console.log(a);
 
-console.log('Var vs LET & CONST');
-{
-var course1 = "C 1";
-let course2 = "c 2";
-const course3 = "c 3";
-console.log(course1);
-console.log(course2);
-console.log(course3);
-}
+// console.log('Var vs LET & CONST');
+// {
+// var course1 = "C 1";
+// let course2 = "c 2";
+// const course3 = "c 3";
+// console.log(course1);
+// console.log(course2);
+// console.log(course3);
+// }
 
-console.log(course1);  //only this shit appear cuz it can get out of the scope
-console.log(course2); // get out of memory == never existed;
-console.log(course3); // get out of memory never existed;
+// console.log(course1);  //only this shit appear cuz it can get out of the scope
+// console.log(course2); // get out of memory == never existed;
+// console.log(course3); // get out of memory never existed;
+
+// console.log(a);
+// var a;
+// a = 10;
+// console.log(a);
+
+// =
+
+// let x = 10;
+// console.log(typeof x); // 'number'
+// console.log(typeof typeof x); //'string'
+
+// const sum = 1 + 2 + '3';
+// console.log(sum); // 33
+
+
+// let score = 99;
+// console.log(score++); // 99, not 101 - in trc, + sau
+// console.log(++score); //101, not 99 - + xong moi in
+
+// const a = 3;
+// const b = '3';
+// console.log(a==b); //true
+// console.log(a===b); //false
+
