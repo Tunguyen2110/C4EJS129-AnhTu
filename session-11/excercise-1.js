1a20e0103250a82eb1d47d778fcf963e91657ae9
Tu Nguyen <tunguyen211098@gmail.com>
2022-07-19 00:30:56 +0700

    update session 11 - day 13

---
+++ session-11/excercise-1.js
@@ -0,0 +1,2 @@
+//BT2: Dùng JS in ra màn hình các số từ 1-99 (gợi ý: dùng vòng lặp)
+
---
+++ session-11/excercise-2.js
@@ -0,0 +1,17 @@
+// by id - return 1 element
+const testId = document.getElementById('testId');
+console.log('testId');
+
+// by class - return array
+
+const testClass = document.getElementsByClassName('testClass');
+console.log('testClass');
+
+//name tag
+const tagP = document.getElementsByTagName('tagP');
+console.log('tagP');
+
+
+//querySelectorAll
+
+//querySelector
